496b36660f4337b7b3070c84c87ea053c57fe0fd
steel9 <steel9apps@gmail.com>
2021-12-04 14:15:22 +0100

    Support for Paper v2 API, rewrite large parts

--- paper-updater.py
+++ paper-updater.py
@@ -9,33 +9,34 @@ import requests
 
 class ServerBuild:
     version_url_template = "https://papermc.io/api/v2/projects/paper/versions/{}"
-    download_url_template = "https://papermc.io/api/v2/projects/paper/versions/{0}/builds/{1}/downloads/paper-{0}-{1}.jar"
+    download_url_template =\
+        "https://papermc.io/api/v2/projects/paper/versions/{0}/builds/{1}/downloads/paper-{0}-{1}.jar"
 
-    def __init__(self, build_num, mc_version, download_url, download_size):
-        self.build_num = build_num
+    def __init__(self, mc_version, build_num, download_size, download_url):
         self.mc_version = mc_version
-        self.download_url = download_url
+        self.build_num = build_num
         self.download_size = download_size
+        self.download_url = download_url
 
     @staticmethod
     def get_latest(mc_version):
         print('Checking for updates ...')
 
-        build_data = json.loads(requests.get(version_url_template.format(mc_version)).text)
+        build_data = json.loads(requests.get(ServerBuild.version_url_template.format(mc_version)).text)
         build_num = build_data['builds'][-1]
 
-        download_url = download_url_template.format(mc_version, build_num)
+        download_url = ServerBuild.download_url_template.format(mc_version, build_num)
         download_headers = requests.head(download_url)
         download_size = int(download_headers.headers['Content-Length'])
 
-        return ServerBuild(download_url, mc_version, build_num, download_size)
+        return ServerBuild(mc_version, build_num, download_size, download_url)
 
-    def prompt_update(self, server_dir):
-        latest_filename = 'paper-{}-{}.jar'.format(self.mc_version, self.build_num)
-        filename_pattern = r"^paper-{}-(.*).jar$".format(self.mc_version)
+    @staticmethod
+    def get_installed(mc_version, server_dir):
+        filename_pattern = r"^paper-{}-(.*).jar$".format(mc_version)
 
-        # Find newest existing build jar in script dir
-        current_build_num = -1
+        # Find the newest existing build jar in script dir
+        latest_installed_build_num = -1
         for filename in os.listdir(server_dir):
             if not re.match(filename_pattern, filename):
                 # file is not a Paper jar file
@@ -46,20 +47,52 @@ class ServerBuild:
             except ValueError:
                 continue
 
-            if build_num > current_build_num:
-                current_build_num = build_num
+            if build_num > latest_installed_build_num:
+                latest_installed_build_num = build_num
 
-        if self.build_num <= current_build_num:
-            print('No updates are available (current build: {})'.format(current_build_num))
+        return ServerBuild(mc_version, latest_installed_build_num, None, None)
 
+    def update_to(self, *, server_dir, start_script_name):
+        # UPDATE SERVER JAR
+
+        installed_build = ServerBuild.get_installed(self.mc_version, server_dir)
+
+        if self.build_num <= installed_build.build_num:
+            print('No updates are available (current build: {})'.format(installed_build.build_num))
+            return
         print('An update is available!')
-
         print('New Build:         {}'.format(self.build_num))
-        if current_build_num > -1:
-            print('Installed Build:   {}'.format(current_build_num))
+        if installed_build.build_num > -1:
+            print('Installed Build:   {}'.format(installed_build.build_num))
         else:
             print('Installed Build:   No installed build was found')
         print('Download Size:     {}'.format(sizeof_fmt(self.download_size)))
+
+        print('Downloading update...')
+        filepath = os.path.join(server_dir, "paper-{}-{}.jar".format(self.mc_version, self.build_num))
+        jar = requests.get(self.download_url, allow_redirects=True)
+        with open(filepath, 'wb') as file:
+            file.write(jar.content)
+
+        # UPDATE SERVER SCRIPT
+
+        jar_pattern_general = r"paper-(.*).jar"
+        print('Updating server script...')
+        start_script_path = os.path.join(server_dir, start_script_name)
+        if os.path.isfile(start_script_path):
+            with open(start_script_path, 'r') as file:
+                start_script = file.read()
+
+            # replace Paper jar filename in script with the filename of the new jar
+            start_script = re.sub(jar_pattern_general, os.path.basename(filepath), start_script)
+
+            with open(start_script_path, 'w') as file:
+                file.write(start_script)
+        else:
+            print("ERROR: Start script path '{}' cannot be found. Please update your server script manually.".format(
+                start_script_path))
+
+        print("Update finished!")
 
 
 def cls():
@@ -73,36 +106,6 @@ def sizeof_fmt(num, suffix='B'):
             return "%3.1f %s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f %s%s" % (num, 'Yi', suffix)
-
-
-def download_update(server_build):
-    print('Downloading update...')
-
-    # Download and save jar
-    jar = requests.get(server_build.download_url, allow_redirects=True)
-    with open(server_build.filepath, 'wb') as file:
-        file.write(jar.content)
-
-
-def update_server_script(server_build, server_dir, start_script_name):
-    jar_pattern = r"paper-(.*).jar"
-
-    print('Updating server script...')
-
-    start_script_path = os.path.join(server_dir, start_script_name)
-    if not os.path.isfile(start_script_path):
-        print("ERROR: Start script path '{}' cannot be found. Please update your server script manually.".format(
-            start_script_path))
-        return
-
-    with open(start_script_path, 'r') as file:
-        filedata = file.read()
-
-    # replace Paper jar filename in script with the filename of the new jar
-    filedata = re.sub(jar_pattern, os.path.basename(server_build.filepath), filedata)
-
-    with open(start_script_path, 'w') as file:
-        file.write(filedata)
 
 
 def print_title(s):
@@ -134,10 +137,7 @@ def main():
         args.server_dir = os.path.expanduser(args.server_dir)
 
     latest_build = ServerBuild.get_latest(args.minecraft_version)
-
-    download_update(server_build)
-    update_server_script(server_build, args.server_dir, args.start_script_name)
-    print('Update finished!')
+    latest_build.update_to(server_dir=args.server_dir, start_script_name=args.start_script_name)
 
 
 main()
